64dd33feb0701f606f65263c8cfb838c4a33506d
Kendra Wilkerson <kwilkerson@ggc.edu>
2022-05-27 11:58:39 -0400

    added route for sign up page

--- db.js
+++ db.js
@@ -8,4 +8,44 @@ const pool = new Pool({
     database= "yumappusers"
 });
 
+//ROUTEYS
+
+//adding a new user to the database
+const newUser = (req, res) => {
+    let insertQuery = `INSERT INTO users (email, password) 
+                        VALUES ('${user.email}', '${user.password}')`
+    const user = req.body;
+
+    //callback
+    pool.query(insertQuery, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            console.log(res.rows[0])
+        }
+    })
+
+    //promise
+    pool
+        .query(insertQuery)
+        .then(res => {
+            console.log(res.rows[0])
+        })
+        .catch(e => console.error(e.stack))
+
+    //async/await 
+    try {
+        const res = await pool.query(insertQuery)
+        console.log(res.rows[0])
+    } catch (err) {
+        console.log(err.stack)
+    }
+
+}
+
+
+
 module.exports = pool;
+module.exports = {
+    newUser
+}
